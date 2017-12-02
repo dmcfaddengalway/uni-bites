accda09449b8a667fe98f236641d96bfd498e508
Daniel McFadden <dmcfaddengalway@gmail.com>
2017-12-02 15:39:24 +0000

    no message

--- public/javascripts/auth.js
+++ public/javascripts/auth.js
@@ -1,4 +1,4 @@
-    $(document).ready(
+$(document).ready(
     function() {
         /**
          * Event handler for when the user attempts to register
@@ -26,3 +26,38 @@
             });
         });
     });
+
+$(document).ready(
+    function() {
+        /**
+         * Event handler for when the user attempts to register
+         */
+        $("#log-form").submit(function (event) {
+            event.preventDefault();
+            $.ajax({
+                type: 'POST',
+                url: '/users/register',
+                dataType: 'json',
+                data: {
+                    'user_name': event.target.inputUsername.value,
+                    'password': event.target.inputPassword.value
+                },
+                success: function(token){
+         $(location).attr('href', '/feed' );	// Redirect to a login page
+                },
+                error: function(errMsg) {
+                    swal(
+                        'Oops...',
+                        errMsg.responseJSON.body,
+                        'error'
+                    )
+                }
+            });
+        });
+    });
+
+$("#logout").click(function (event) {
+
+	Cookies.remove('Authorization');
+	$(location).attr('href', '/');
+});
